d9b1de07fa1c13630aa2f3af4011ec4fb2a31ef9
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2025-04-22 22:28:54 +0200

    added browser router and imported components

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import AppLayout from './AppLayout';
+import Stock from '../features/stock/Stock';
+import Categories from '../features/categories/Categories';
+import Categorie from '../features/categories/Categorie';
+import Recipes from '../features/recipes/Recipes';
+import NewRecipeForm from '../components/NewRecipeForm';
+import Search from '../features/search/Search';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout/>}>
+          <Route path="stock" element={<Stock/>} />
+          <Route path="categories" element={<Categories/>} />
+          <Route path="categorie" element={<Categorie/>} />
+          <Route path="recipes" element={<Recipes/>} />
+          <Route path="recipes/new" element={<NewRecipeForm/>} />
+          <Route path="search" element={<Search/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
