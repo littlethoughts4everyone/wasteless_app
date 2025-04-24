7bf42ed29cf574a0522ee51b46b0eb03e2f51686
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2025-04-24 22:34:21 +0200

    started writing stcok-component, added food data

---
+++ src/data/foodData.js
@@ -0,0 +1,22 @@
+export const FOOD_CATEGORIES = [
+    Fleisch/Fisch,
+    Gemüse/Obst,
+    Molkereiprodukte,
+    Konserven,
+    Grundnahrungsmittel,
+    Gewürze,
+    Saucen,
+    Getränke/Spirituosen
+];
+
+export const RECIPE_CATEGORIES = [
+    Aufläufe,
+    Currys,
+    Fleischgerichte,
+    Fischgerichte,
+    Nudelgerichte,
+    Pizza,
+    Risottos,
+    Suppen,
+    Vorspeisen
+];
