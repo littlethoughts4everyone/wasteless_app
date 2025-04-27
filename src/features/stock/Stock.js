5cea3e328c288ce3d195ab7a9e8c54908db62f28
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2025-04-27 15:48:10 +0200

    adjusted stock component

--- src/features/stock/Stock.js
+++ src/features/stock/Stock.js
@@ -17,27 +17,51 @@ export default function Stock() {
                 <h1>Stock:</h1>
                 <div className="stock-category">
                     <h2>Meat/Fish:</h2>
+                    <div id="meatfish">
+
+                    </div>
                 </div>
                 <div className="stock-category">
                     <h2>Vegetables/Fruits:</h2>
+                    <div id="vegetablesfruits">
+
+                    </div>
                 </div>
                 <div className="stock-category">
                     <h2>Dairy:</h2>
+                    <div id="dairy">
+
+                    </div>
                 </div>
                 <div className="stock-category">
                     <h2>Canned Goods:</h2>
+                    <div id="cannedgoods">
+
+                    </div>
                 </div>
                 <div className="stock-category">
                     <h2>Basic Foods:</h2>
+                    <div id="basicfoods">
+
+                    </div>
                 </div>
                 <div className="stock-category">
                     <h2>Spices:</h2>
+                    <div id="spices">
+
+                    </div>
                 </div>
                 <div className="stock-category">
                     <h2>Sauces:</h2>
+                    <div id="sauces">
+
+                    </div>
                 </div>
                 <div className="stock-category">
-                    <h2>Beverages/Spirits:</h2>
+                    <h2>Beverages:</h2>
+                    <div id="beverages">
+
+                    </div>
                 </div>
             </section>
         </div>
