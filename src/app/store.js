2b1238946a505771f9fc253190212a5e41cfda0f
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2025-04-27 15:16:15 +0200

    adjust the redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import stockReducer from "../features/stock/stockSlice";
+import foodItemsReducer from "../features/foodItem/foodItemSlice";
 import categoriesReducer from "../features/categories/categoriesSlice";
 import recipesReducer from "../features/recipes/recipesSlice";
 import searchReducer from "../features/search/searchSlice";
@@ -7,6 +8,7 @@ import searchReducer from "../features/search/searchSlice";
 export default configureStore({
     reducer: {
         allStock: stockReducer,
+        allFoodItems: foodItemsReducer,
         allCategories: categoriesReducer,
         allRecipes: recipesReducer,
         search: searchReducer
