2c6d66ec9ae2d42fd27d26ce43a27da6448fa5b2
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2025-04-29 22:41:48 +0200

    rendered the first stock category

--- src/features/stock/Stock.js
+++ src/features/stock/Stock.js
@@ -2,7 +2,6 @@ import react from "react";
 import NewStockItemForm from "../../components/NewStockItemForm";
 import { useSelector } from "react-redux";
 import { selectAllStockItems } from "./stockItemsSlice";
-import StockItem from "./StockItem";
 
 export default function Stock() {
 
@@ -17,17 +16,26 @@ export default function Stock() {
             <section className="stock-container">
                 <h1>Stock:</h1>
                 <div className="stock-category">
-                    <h2>Meat &sol Fish:</h2>
+                    <h2>Meat / Fish:</h2>
                     <div id="meatfish">
-                        <ul>
-                            {Object.values(stockItems).map((stockItem) => (
-                                    <StockItem/>
+                        <ul className="items-list">
+                            {Object.values(stockItems)
+                            .filter((item) => item.category === "Meat / Fish")
+                            .map((stockItem) => (
+                                    <li className="stock-item" key={stockItem.id}>
+                                        <div>
+                                            <p className="item-amount">{stockItem.amount}</p>
+                                            <p className="item-unit">{stockItem.unit}</p>
+                                            <p className="item-name">{stockItem.name}</p>
+                                            <button>X</button>
+                                        </div>
+                                    </li>
                                 ))}
                         </ul>
                     </div>
                 </div>
                 <div className="stock-category">
-                    <h2>Vegetables &sol Fruits:</h2>
+                    <h2>Vegetables / Fruits:</h2>
                     <div id="vegetablesfruits">
 
                     </div>
