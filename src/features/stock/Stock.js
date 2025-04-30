b3dd65b4f341669f9aa7b48dfcfba68b0949ff2f
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2025-04-30 22:53:52 +0200

    completed category lists

--- src/features/stock/Stock.js
+++ src/features/stock/Stock.js
@@ -1,5 +1,6 @@
 import react from "react";
 import NewStockItemForm from "../../components/NewStockItemForm";
+import StockItem from "./StockItem";
 import { useSelector } from "react-redux";
 import { selectAllStockItems } from "./stockItemsSlice";
 
@@ -17,63 +18,114 @@ export default function Stock() {
                 <h1>Stock:</h1>
                 <div className="stock-category">
                     <h2>Meat / Fish:</h2>
-                    <div id="meatfish">
+                    <div id="meat-fish">
                         <ul className="items-list">
                             {Object.values(stockItems)
                             .filter((item) => item.category === "Meat / Fish")
                             .map((stockItem) => (
-                                    <li className="stock-item" key={stockItem.id}>
-                                        <div>
-                                            <p className="item-amount">{stockItem.amount}</p>
-                                            <p className="item-unit">{stockItem.unit}</p>
-                                            <p className="item-name">{stockItem.name}</p>
-                                            <button>X</button>
-                                        </div>
-                                    </li>
-                                ))}
+                                <li className="stock-item" key={stockItem.id}>
+                                    <StockItem amount={stockItem.amount} unit={stockItem.unit} name={stockItem.name}/>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
                 <div className="stock-category">
                     <h2>Vegetables / Fruits:</h2>
-                    <div id="vegetablesfruits">
-
+                    <div id="vegetables-fruits">
+                        <ul className="items-list">
+                            {Object.values(stockItems)
+                            .filter((item) => item.category === "Vegetables / Fruits")
+                            .map((stockItem) => (
+                                <li className="stock-item" key={stockItem.id}>
+                                    <StockItem amount={stockItem.amount} unit={stockItem.unit} name={stockItem.name}/>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className="stock-category">
                     <h2>Dairy:</h2>
                     <div id="dairy">
-
+                        <ul className="items-list">
+                            {Object.values(stockItems)
+                            .filter((item) => item.category === "Dairy")
+                            .map((stockItem) => (
+                                <li className="stock-item" key={stockItem.id}>
+                                    <StockItem amount={stockItem.amount} unit={stockItem.unit} name={stockItem.name}/>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className="stock-category">
-                    <h2>Canned Goods:</h2>
-                    <div id="cannedgoods">
-
+                    <h2>Canned Food:</h2>
+                    <div id="canned-food">
+                        <ul className="items-list">
+                            {Object.values(stockItems)
+                            .filter((item) => item.category === "Canned Food")
+                            .map((stockItem) => (
+                                <li className="stock-item" key={stockItem.id}>
+                                    <StockItem amount={stockItem.amount} unit={stockItem.unit} name={stockItem.name}/>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className="stock-category">
                     <h2>Basic Foods:</h2>
-                    <div id="basicfoods">
-
+                    <div id="basic-foods">
+                        <ul className="items-list">
+                            {Object.values(stockItems)
+                            .filter((item) => item.category === "Basic Foods")
+                            .map((stockItem) => (
+                                <li className="stock-item" key={stockItem.id}>
+                                    <StockItem amount={stockItem.amount} unit={stockItem.unit} name={stockItem.name}/>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className="stock-category">
                     <h2>Spices:</h2>
                     <div id="spices">
-
+                        <ul className="items-list">
+                            {Object.values(stockItems)
+                            .filter((item) => item.category === "Spices")
+                            .map((stockItem) => (
+                                <li className="stock-item" key={stockItem.id}>
+                                    <StockItem amount={stockItem.amount} unit={stockItem.unit} name={stockItem.name}/>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className="stock-category">
                     <h2>Sauces:</h2>
                     <div id="sauces">
-
+                        <ul className="items-list">
+                            {Object.values(stockItems)
+                            .filter((item) => item.category === "Sauces")
+                            .map((stockItem) => (
+                                <li className="stock-item" key={stockItem.id}>
+                                    <StockItem amount={stockItem.amount} unit={stockItem.unit} name={stockItem.name}/>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className="stock-category">
                     <h2>Beverages:</h2>
                     <div id="beverages">
-
+                        <ul className="items-list">
+                            {Object.values(stockItems)
+                            .filter((item) => item.category === "Beverages")
+                            .map((stockItem) => (
+                                <li className="stock-item" key={stockItem.id}>
+                                    <StockItem amount={stockItem.amount} unit={stockItem.unit} name={stockItem.name}/>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </section>
