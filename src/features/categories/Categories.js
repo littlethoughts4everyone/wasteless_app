78d4e41fdb026c6c4a8beaf76f3b89a0e08ba64f
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2025-05-05 22:27:11 +0200

    wrote categories feature

---
+++ src/features/categories/Categories.js
@@ -0,0 +1,24 @@
+import react from "react";
+import { Link } from "react-router-dom";
+import ROUTES from "../../app/routes";
+import { RECIPE_CATEGORIES } from "../../data/foodData";
+
+export default function Categories() {
+    return (
+        <section>
+            <div className="categories-container">
+                {RECIPE_CATEGORIES.map((category) => {
+                    return (
+                        <div className="recipe-category" key={category.id}>
+                            <Link to={ROUTES.categoryRoute(category.id)} className="category-link">
+                                <div className={category.name.toLowerCase().replace(/[^a-z]/g, "")}>
+                                    <h2>{category.name}</h2>
+                                </div>
+                            </Link>
+                        </div>
+                    );
+                })}
+            </div>
+        </section>
+    )
+}
