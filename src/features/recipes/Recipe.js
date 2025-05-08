1b878f90f86a627fc62b0e6e8e1a4d0079f9f9cb
mweiher <149411694+littlethoughts4everyone@users.noreply.github.com>
2025-05-08 22:11:30 +0200

    wrote component

---
+++ src/features/recipes/Recipe.js
@@ -0,0 +1,19 @@
+import react from "react";
+import { useSelector } from "react-redux";
+import Ingredient from "../ingredients/Ingredient";
+import {selectRecipe} from "./recipesSlice";
+
+export default function Recipe({id}) {
+    const recipe = useSelector(selectRecipe(id));
+
+    return (
+        <section>
+            <h2 className="recipe-name">{recipe.name}</h2>
+            <ul className="ingredients-list">
+                {recipe.ingredientIds.map((id) => (
+                    <Ingredient key={id} id={id}/>
+                ))}
+            </ul>
+        </section>
+    )
+}
